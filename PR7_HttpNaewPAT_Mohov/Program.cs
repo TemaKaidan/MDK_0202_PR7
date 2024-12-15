ffd4a33ab997957d372485ee3421d3d5e1a2bf43
Artem <artemmohov05@mail.ru>
2024-12-15 21:56:54 +0500

    Получение информации с сайта. Мохов

--- PR7_HttpNaewPAT_Mohov/Program.cs
+++ PR7_HttpNaewPAT_Mohov/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,19 @@ namespace PR7_HttpNaewPAT_Mohov
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-
+            WebRequest request = WebRequest.Create("");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string data = reader.ReadToEnd();
+            Console.WriteLine(data);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
